56058c1616d41fa388517518cbd9ee7d59842591
Ahmad34-90 <ahmiiikha@gmail.com>
2026-09-19 21:35:23 -0700

    add brightness adjustment

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Numpy Image manipulation tool")
+from PIL import Image
+import numpy as np
+
+images = Image.open("images/input.jpg")
+
+#img to numpy array
+image_array = np.array(images)
+print("Origional shape = ", image_array.shape)
+print("Origional datatype = ", image_array.dtype)
+
+# Increase brightness
+brighter_array = image_array-100
+
+#value between 0 and 255
+brighter_array = np.clip(brighter_array, 0, 255)
+
+#convert numpy array back to image
+brighter_image = Image.fromarray(brighter_array.astype(np.uint8))
+
+brighter_image.save("output/brighter.jpg")
+print("Brighter image Save.")
